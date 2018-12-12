d34aca9974a625604512639ab50fd5c76abbd73d
Andreas Hörberg <andreas.horberg@daresay.co>
2018-12-12 17:34:20 +0100

    Initial Commit

---
+++ Kalkera/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Kalkera
+//
+//  Created by Andreas Hörberg on 2018-12-12.
+//  Copyright © 2018 Andreas Hörberg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
